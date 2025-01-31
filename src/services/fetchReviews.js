9f9c5a1acea8a904a6203b89eef3423ac5f0fed5
AlexCode-dot <alle7000.andersson@gmail.com>
2025-01-31 11:10:41 +0100

    - changed my fetch function so it can be used in other tasks - made a new route file where all our REST-api routes will be put - changed the logic with the pagesize function

--- src/services/fetchReviews.js
+++ src/services/fetchReviews.js
@@ -8,25 +8,21 @@ function toReviewObject(apiObject) {
 }
 
 const cmsAdapter = {
-  loadReviewsForMovie: async (movieId, page) => {
-    const pageSize = await getReviewsPageSize(movieId, page)
-    const res = await fetch(
-      `https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=${page}&pagination[pageSize]=${pageSize}`
-    )
+  loadReviewsForMovie: async (movieId, page, pageSize) => {
+    let apiUrl = `https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=${page}`
+
+    if (pageSize) {
+      apiUrl += `&pagination[pageSize]=${pageSize}`
+    }
+
+    const res = await fetch(apiUrl)
     const payload = await res.json()
+
     return {
       data: payload.data.map(toReviewObject),
       meta: payload.meta,
     }
   },
-}
-
-export async function getReviewsPageSize(movieId, page) {
-  const res = await fetch(
-    `https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=${page}`
-  )
-  const payload = await res.json()
-  return payload.meta.pagination.pageSize
 }
 
 export default cmsAdapter
