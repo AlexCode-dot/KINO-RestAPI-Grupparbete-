3297a32a0a1b3a17e3371ebabc03f9a0122baee7
RobTrb <robert.trb@gmail.com>
2025-01-31 13:22:50 +0100

    have added static objects in detailpages/screenings in preparation and styled them in prep for dynamic creation via function

---
+++ static/Script/createScreeningsDOM.js
@@ -0,0 +1,49 @@
+const mockDatesTimes = {
+  data: [
+    {
+      id: 296,
+      attributes: {
+        start_time: '2025-02-14T17:00:00.000Z',
+        room: 'Stora salongen',
+        createdAt: '2025-01-29T14:19:16.064Z',
+        updatedAt: '2025-01-29T14:19:16.064Z',
+      },
+    },
+    {
+      id: 295,
+      attributes: {
+        start_time: '2025-02-15T15:00:00.000Z',
+        room: 'Stora salongen',
+        createdAt: '2025-01-29T14:19:16.064Z',
+        updatedAt: '2025-01-29T14:19:16.064Z',
+      },
+    },
+    {
+      id: 293,
+      attributes: {
+        start_time: '2025-02-16T16:00:00.000Z',
+        room: 'Stora salongen',
+        createdAt: '2025-01-29T14:19:16.064Z',
+        updatedAt: '2025-01-29T14:19:16.064Z',
+      },
+    },
+    {
+      id: 291,
+      attributes: {
+        start_time: '2025-02-17T19:00:00.000Z',
+        room: 'Stora salongen',
+        createdAt: '2025-01-29T14:19:16.064Z',
+        updatedAt: '2025-01-29T14:19:16.064Z',
+      },
+    },
+    {
+      id: 298,
+      attributes: {
+        start_time: '2025-02-18T14:00:00.000Z',
+        room: 'Stora salongen',
+        createdAt: '2025-01-29T14:19:16.064Z',
+        updatedAt: '2025-01-29T14:19:16.064Z',
+      },
+    },
+  ],
+}
