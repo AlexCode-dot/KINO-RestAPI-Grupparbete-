a0fefed057f8f92c5efcfbe78d3cca2d8bdfef00
Deansie <dennis.andersen@lerniastudent.se>
2025-01-31 11:20:39 +0100

    Add screening js-files, adjusted fetch function

--- src/services/fetchScreenings.js
+++ src/services/fetchScreenings.js
@@ -15,8 +15,15 @@ function toScreeningObject(apiObjekt) {
 
 const screeningAdapter = {
   loadScreeningForMovie: async (movieId) => {
+    const now = new Date()
+    const fiveDaysLater = new Date(now)
+    fiveDaysLater.setDate(now.getDate() + 5)
+
     const res = await fetch(
-      `https://plankton-app-xhkom.ondigitalocean.app/api/screenings?populate=movie&filters[movie]=${movieId}`
+      `https://plankton-app-xhkom.ondigitalocean.app/api/screenings?populate=movie&filters[movie]=${movieId}
+      &filters[start_time][$gte]=${now.toISOString()}
+      &filters[start_time][$lte]=${fiveDaysLater.toISOString()}
+      &pagination[limit]=10`
     )
     const payload = await res.json()
     return {
@@ -25,5 +32,5 @@ const screeningAdapter = {
     }
   },
 }
-console.log(screeningAdapter)
+
 export default screeningAdapter
