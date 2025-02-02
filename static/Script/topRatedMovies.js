7f5145472ad51689b52b4e89a6ab66b9179887a0
AlexCode-dot <alle7000.andersson@gmail.com>
2025-02-02 14:45:58 +0100

    - added logic to render top rated movie to the DOM, only when path name is "/"

--- static/Script/topRatedMovies.js
+++ static/Script/topRatedMovies.js
@@ -1,5 +1,48 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  if (window.page !== 'hem') {
+  if (window.location.pathname !== '/') {
     return
   }
+
+  try {
+    const response = await fetch('/api/movies/top-rated')
+
+    if (!response.ok) {
+      const errorData = await response.json()
+      throw new Error(errorData.error)
+    }
+
+    const topRatedMovies = await response.json()
+    createtopRatedMovies(topRatedMovies)
+  } catch (error) {
+    console.error('Fel vid hämtning av filmer med högst betyg:', error.message)
+  }
 })
+
+async function createtopRatedMovies(movies) {
+  const moviesContainer = document.querySelector('.top-rated__container')
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement('li')
+    movieElement.classList.add('top-rated__movie')
+    moviesContainer.appendChild(movieElement)
+
+    const movieLink = document.createElement('a')
+    movieLink.setAttribute('href', `/movies/${movie.id}`)
+    movieElement.appendChild(movieLink)
+
+    const movieImage = document.createElement('img')
+    movieImage.classList.add('top-rated__movie-image')
+    movieImage.setAttribute('src', movie.image.url)
+    movieLink.appendChild(movieImage)
+
+    const movieTitle = document.createElement('h4')
+    movieTitle.classList.add('top-rated__movie-title')
+    movieTitle.textContent = movie.title
+    movieLink.appendChild(movieTitle)
+
+    const movieRating = document.createElement('p')
+    movieRating.classList.add('top-rated__movie-rating')
+    movieRating.textContent = movie.averageRating
+    movieLink.appendChild(movieRating)
+  })
+}
