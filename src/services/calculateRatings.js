db06e118af596f41223eecb62b948c56fee446f2
Calle <ca.karlsson@hotmail.com>
2025-02-04 14:17:59 +0100

    Update calculateRatings to count more reviews into average number

--- src/services/calculateRatings.js
+++ src/services/calculateRatings.js
@@ -1,11 +1,12 @@
 import cmsAdapter from './fetchReviews.js'
 
 export async function calculateAverageRating(movieId) {
-  const reviews = await cmsAdapter.loadReviewsForMovie(movieId, 1, 5)
+  const reviews = await cmsAdapter.loadReviewsForMovie(movieId, 1, 100)
   const ratings = reviews.data.map((review) => review.rating)
+  console.log(ratings)
   const totalRatings = ratings.reduce((acc, rating) => acc + rating, 0)
   const averageRating = ratings.length ? totalRatings / ratings.length : 0
-  console.log(`Reviews: ${JSON.stringify(ratings)}`)
+  console.log(`Reviews: ${ratings}`)
   console.log(`Average rating for movie ${movieId}: ${averageRating}`)
   return averageRating
 }
