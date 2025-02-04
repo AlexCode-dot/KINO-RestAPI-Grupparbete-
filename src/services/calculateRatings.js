b1d5c91707127f575687653fe649fa9f263b5d37
Calle <ca.karlsson@hotmail.com>
2025-02-04 16:14:16 +0100

    Added debug option for logs in calculateAverageRating

--- src/services/calculateRatings.js
+++ src/services/calculateRatings.js
@@ -3,10 +3,13 @@ import cmsAdapter from './fetchReviews.js'
 export async function calculateAverageRating(movieId) {
   const reviews = await cmsAdapter.loadReviewsForMovie(movieId, 1, 100)
   const ratings = reviews.data.map((review) => review.rating)
-  console.log(ratings)
   const totalRatings = ratings.reduce((acc, rating) => acc + rating, 0)
   const averageRating = ratings.length ? totalRatings / ratings.length : 0
-  console.log(`Reviews: ${ratings}`)
-  console.log(`Average rating for movie ${movieId}: ${averageRating}`)
+  const debug = true
+  if (debug === false) {
+    console.log(`Reviews: ${ratings}`)
+    console.log(`Average rating for movie ${movieId}: ${averageRating}`)
+  }
+
   return averageRating
 }
