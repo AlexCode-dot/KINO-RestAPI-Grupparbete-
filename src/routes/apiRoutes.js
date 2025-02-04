f1c71d9d8ce20e65556cbdadafb88e7543866b8b
Calle <ca.karlsson@hotmail.com>
2025-02-04 11:32:45 +0100

    Add endpoint to calculate average rating for a movie

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getTopRatedMoviesByRating } from '../services/moviesTopRated.js'
 import cmsAdapter from '../services/fetchReviews.js'
+import { calculateAverageRating } from '../services/calculateRatings.js'
 
 const router = express.Router()
 
@@ -11,6 +12,17 @@ export default function apiRoutes(api) {
       response.json(topRated)
     } catch (err) {
       next(err)
+    }
+  })
+
+  router.get('/average-rating/:movieId', async (req, res) => {
+    try {
+      console.log('Calculating average rating')
+      const movieId = req.params.movieId
+      const averageRating = await calculateAverageRating(movieId)
+      res.json({ averageRating })
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to calculate average rating' })
     }
   })
 
