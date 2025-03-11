0380847ed245c908a2f3eb89c3e315ec3aae6742
AlexCode-dot <alle7000.andersson@gmail.com>
2025-03-11 18:32:19 +0100

    - fixed feedback for user if login succesfull or not - fixed some broken code

--- static/Script/compareFormToLocalStorage.js
+++ static/Script/compareFormToLocalStorage.js
@@ -1,27 +1,26 @@
 const loginForm = document.querySelector('form')
+
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const usernameInput = document.querySelector('#exampleInputEmail1')
   const passwordInput = document.querySelector('#exampleInputPassword1')
 
-  const enteredUsername = document.querySelector('#exampleInputEmail1').value
-  const enteredPassword = document.querySelector('#exampleInputPassword1').value
-
-  const storedUsername = localStorage.getItem('username')
-  const storedPassword = localStorage.getItem('password')
+  const enteredUsername = usernameInput.value
+  const enteredPassword = passwordInput.value
 
   const storedMembers = JSON.parse(localStorage.getItem('members')) || []
 
-  const matchedUser =
-    storedMembers.find((user) => user.username === enteredUsername && user.password === enteredPassword) ||
-    user.email === enteredUser
+  const matchedUser = storedMembers.find(
+    (user) => (user.username === enteredUsername || user.email === enteredUsername) && user.password === enteredPassword
+  )
 
   if (matchedUser) {
-    window.location.href = '/profile'
-  } else if (enteredUsername !== '' && enteredPassword !== '' && !matchedUser) {
-    alert('Wrong username or password')
-  } else if (enteredUsername === '' && enteredPassword === '') {
-    return
+    localStorage.setItem('loggedInUser', JSON.stringify(matchedUser))
+    document.getElementById('loginMessage').style.display = 'none'
+
+    const myModal = new bootstrap.Modal(document.getElementById('loggedInModal'))
+    myModal.show()
+  } else {
+    document.getElementById('loginMessage').style.display = 'block'
   }
 })
