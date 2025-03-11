de7fba7d1d510fac30334c29b3120033006e7c96
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-12 00:26:02 +0100

    last try to get tailwind to work

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
-  
-  
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// Configuration basics for tailwind generated
+module.exports = {
+  content: ['./templates/**/*.{html,ejs}', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
