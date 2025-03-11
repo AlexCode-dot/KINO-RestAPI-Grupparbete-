ad155c56eeb8aa534fca4c096fa3a6aba9474ba6
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-12 00:39:46 +0100

    problems with config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // Configuration basics for tailwind generated
 module.exports = {
-  content: ['./templates/**/*.{html,ejs}', './src/**/*.{js,jsx}'],
+  content: ['./templates/**/*.{html,ejs}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
